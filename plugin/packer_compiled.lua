4649e0dac38e4acb0580f5d97ea429b4e32bf027
yxshv <yashverma30115@gmail.com>
2024-01-05 12:30:26 +0530

    change keybinding of opening and closing terminal to ctril+space

--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -49,8 +49,8 @@ local function save_profiles(threshold)
 end
 
 time([[Luarocks path setup]], true)
-local package_path_str = "/home/yxsh/.cache/nvim/packer_hererocks/2.1.0-beta3/share/lua/5.1/?.lua;/home/yxsh/.cache/nvim/packer_hererocks/2.1.0-beta3/share/lua/5.1/?/init.lua;/home/yxsh/.cache/nvim/packer_hererocks/2.1.0-beta3/lib/luarocks/rocks-5.1/?.lua;/home/yxsh/.cache/nvim/packer_hererocks/2.1.0-beta3/lib/luarocks/rocks-5.1/?/init.lua"
-local install_cpath_pattern = "/home/yxsh/.cache/nvim/packer_hererocks/2.1.0-beta3/lib/lua/5.1/?.so"
+local package_path_str = "/Users/balkrishnaverma/.cache/nvim/packer_hererocks/2.1.1700008891/share/lua/5.1/?.lua;/Users/balkrishnaverma/.cache/nvim/packer_hererocks/2.1.1700008891/share/lua/5.1/?/init.lua;/Users/balkrishnaverma/.cache/nvim/packer_hererocks/2.1.1700008891/lib/luarocks/rocks-5.1/?.lua;/Users/balkrishnaverma/.cache/nvim/packer_hererocks/2.1.1700008891/lib/luarocks/rocks-5.1/?/init.lua"
+local install_cpath_pattern = "/Users/balkrishnaverma/.cache/nvim/packer_hererocks/2.1.1700008891/lib/lua/5.1/?.so"
 if not string.find(package.path, package_path_str, 1, true) then
   package.path = package.path .. ';' .. package_path_str
 end
@@ -76,87 +76,87 @@ time([[Defining packer_plugins]], true)
 _G.packer_plugins = {
   LuaSnip = {
     loaded = true,
-    path = "/home/yxsh/.local/share/nvim/site/pack/packer/start/LuaSnip",
+    path = "/Users/balkrishnaverma/.local/share/nvim/site/pack/packer/start/LuaSnip",
     url = "https://github.com/L3MON4D3/LuaSnip"
   },
   ["bufferline.nvim"] = {
     loaded = true,
-    path = "/home/yxsh/.local/share/nvim/site/pack/packer/start/bufferline.nvim",
+    path = "/Users/balkrishnaverma/.local/share/nvim/site/pack/packer/start/bufferline.nvim",
     url = "https://github.com/akinsho/bufferline.nvim"
   },
   ["cmp-buffer"] = {
     loaded = true,
-    path = "/home/yxsh/.local/share/nvim/site/pack/packer/start/cmp-buffer",
+    path = "/Users/balkrishnaverma/.local/share/nvim/site/pack/packer/start/cmp-buffer",
     url = "https://github.com/hrsh7th/cmp-buffer"
   },
   ["cmp-nvim-lsp"] = {
     loaded = true,
-    path = "/home/yxsh/.local/share/nvim/site/pack/packer/start/cmp-nvim-lsp",
+    path = "/Users/balkrishnaverma/.local/share/nvim/site/pack/packer/start/cmp-nvim-lsp",
     url = "https://github.com/hrsh7th/cmp-nvim-lsp"
   },
   ["kanagawa.nvim"] = {
     loaded = true,
-    path = "/home/yxsh/.local/share/nvim/site/pack/packer/start/kanagawa.nvim",
+    path = "/Users/balkrishnaverma/.local/share/nvim/site/pack/packer/start/kanagawa.nvim",
     url = "https://github.com/rebelot/kanagawa.nvim"
   },
   ["lspkind.nvim"] = {
     loaded = true,
-    path = "/home/yxsh/.local/share/nvim/site/pack/packer/start/lspkind.nvim",
+    path = "/Users/balkrishnaverma/.local/share/nvim/site/pack/packer/start/lspkind.nvim",
     url = "https://github.com/onsails/lspkind.nvim"
   },
   ["lualine.nvim"] = {
     loaded = true,
-    path = "/home/yxsh/.local/share/nvim/site/pack/packer/start/lualine.nvim",
+    path = "/Users/balkrishnaverma/.local/share/nvim/site/pack/packer/start/lualine.nvim",
     url = "https://github.com/nvim-lualine/lualine.nvim"
   },
   ["mason.nvim"] = {
     loaded = true,
-    path = "/home/yxsh/.local/share/nvim/site/pack/packer/start/mason.nvim",
+    path = "/Users/balkrishnaverma/.local/share/nvim/site/pack/packer/start/mason.nvim",
     url = "https://github.com/williamboman/mason.nvim"
   },
   ["nvim-cmp"] = {
     loaded = true,
-    path = "/home/yxsh/.local/share/nvim/site/pack/packer/start/nvim-cmp",
+    path = "/Users/balkrishnaverma/.local/share/nvim/site/pack/packer/start/nvim-cmp",
     url = "https://github.com/hrsh7th/nvim-cmp"
   },
   ["nvim-lspconfig"] = {
     loaded = true,
-    path = "/home/yxsh/.local/share/nvim/site/pack/packer/start/nvim-lspconfig",
+    path = "/Users/balkrishnaverma/.local/share/nvim/site/pack/packer/start/nvim-lspconfig",
     url = "https://github.com/neovim/nvim-lspconfig"
   },
   ["nvim-treesitter"] = {
     loaded = true,
-    path = "/home/yxsh/.local/share/nvim/site/pack/packer/start/nvim-treesitter",
+    path = "/Users/balkrishnaverma/.local/share/nvim/site/pack/packer/start/nvim-treesitter",
     url = "https://github.com/nvim-treesitter/nvim-treesitter"
   },
   ["nvim-web-devicons"] = {
     loaded = true,
-    path = "/home/yxsh/.local/share/nvim/site/pack/packer/start/nvim-web-devicons",
+    path = "/Users/balkrishnaverma/.local/share/nvim/site/pack/packer/start/nvim-web-devicons",
     url = "https://github.com/nvim-tree/nvim-web-devicons"
   },
   ["packer.nvim"] = {
     loaded = true,
-    path = "/home/yxsh/.local/share/nvim/site/pack/packer/start/packer.nvim",
+    path = "/Users/balkrishnaverma/.local/share/nvim/site/pack/packer/start/packer.nvim",
     url = "https://github.com/wbthomason/packer.nvim"
   },
   ["plenary.nvim"] = {
     loaded = true,
-    path = "/home/yxsh/.local/share/nvim/site/pack/packer/start/plenary.nvim",
+    path = "/Users/balkrishnaverma/.local/share/nvim/site/pack/packer/start/plenary.nvim",
     url = "https://github.com/nvim-lua/plenary.nvim"
   },
   ["telescope-file-browser.nvim"] = {
     loaded = true,
-    path = "/home/yxsh/.local/share/nvim/site/pack/packer/start/telescope-file-browser.nvim",
+    path = "/Users/balkrishnaverma/.local/share/nvim/site/pack/packer/start/telescope-file-browser.nvim",
     url = "https://github.com/nvim-telescope/telescope-file-browser.nvim"
   },
   ["telescope.nvim"] = {
     loaded = true,
-    path = "/home/yxsh/.local/share/nvim/site/pack/packer/start/telescope.nvim",
+    path = "/Users/balkrishnaverma/.local/share/nvim/site/pack/packer/start/telescope.nvim",
     url = "https://github.com/nvim-telescope/telescope.nvim"
   },
   ["vim-floaterm"] = {
     loaded = true,
-    path = "/home/yxsh/.local/share/nvim/site/pack/packer/start/vim-floaterm",
+    path = "/Users/balkrishnaverma/.local/share/nvim/site/pack/packer/start/vim-floaterm",
     url = "https://github.com/voldikss/vim-floaterm"
   }
 }
--- plugin/terminal.lua
+++ plugin/terminal.lua
@@ -1,1 +1,1 @@
-vim.keymap.set("n", "<c-_>", "<cmd>FloatermNew --width=0.7 --height=0.8 --title=⠀⠀Terminal⠀<cr>")
+vim.keymap.set("n", "<c-space>", "<cmd>FloatermNew --width=0.7 --height=0.8 --title=⠀⠀Terminal⠀<cr>")
