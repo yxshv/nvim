3c38805949d15c90625c66ffbdd15c0b0d0e3c5e
yxshv <yashverma30115@gmail.com>
2026-07-02 20:45:41 +0530

    added system clipboard support

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -13,3 +13,7 @@ map({ "n", "v" }, "<leader>y", '"+y', { desc = "Copy to system clipboard" })
 map("n", "<leader>Y", '"+Y', { desc = "Copy line to system clipboard" })
 map({ "n", "v" }, "<leader>p", '"+p', { desc = "Paste from system clipboard" })
 map({ "n", "v" }, "<leader>P", '"+P', { desc = "Paste before from system clipboard" })
+
+map("n", "<leader>ll", "<cmd>Lazy<cr>", { desc = "Lazy plugins" })
+map("n", "<leader>mm", "<cmd>Mason<cr>", { desc = "Mason tools" })
+map("n", "<leader>qq", "<cmd>qa<cr>", { desc = "Quit Neovim" })
